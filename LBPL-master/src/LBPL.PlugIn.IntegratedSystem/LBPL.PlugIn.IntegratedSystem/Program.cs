b85c734abfb4aa45d65c49287ed19f4059b0038a
linbin524 <244875489@qq.com>
2017-02-28 17:10:30 +0800

    1

--- LBPL-master/src/LBPL.PlugIn.IntegratedSystem/LBPL.PlugIn.IntegratedSystem/Program.cs
+++ LBPL-master/src/LBPL.PlugIn.IntegratedSystem/LBPL.PlugIn.IntegratedSystem/Program.cs
@@ -17,6 +17,7 @@ namespace LBPL.PlugIn.IntegratedSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new WinFormServiceDemo());
+
         }
     }
 }
